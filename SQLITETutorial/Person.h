79a9bae45e704f0a47649465c827dc7d9f0ed022
AabidMulani <mulaniaabid@gmail.com>
2016-02-17 01:21:57 +0530

    Initial Commit

---
+++ SQLITETutorial/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  SQLITETutorial
+//
+//  Created by Magneto on 7/26/14.
+//  Copyright (c) 2014 Magneto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ SQLITETutorial/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  SQLITETutorial
+//
+//  Created by Magneto on 7/26/14.
+//  Copyright (c) 2014 Magneto. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
